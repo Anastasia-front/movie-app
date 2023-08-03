b24667ea26c6fe93c4047f31dbb6c277c4599d38
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-08-03 16:40:22 +0300

    refactor: change name and homepage in package.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import 'modern-normalize';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
-import 'modern-normalize';
 import './index.css';
 
 const rootElement = document.getElementById('root');
